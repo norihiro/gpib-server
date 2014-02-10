05f701550c425907348d18fa30dd2fd64c911170
Norihiro Kamae <kamae@vlsi.kuee.kyoto-u.ac.jp>
2014-02-10 22:59:46 +0900

    device-serial: bugfix: failure to open ignored
    Previously it ignores failure of CreateFile.
    With this update, it will be correctly handled.

--- device-serial.cc
+++ device-serial.cc
@@ -15,6 +15,8 @@ class serial : public device_s
 			char s[64]={0}; snprintf(s, sizeof(s)-1, "\\\\.\\%s", n);
 			handle = CreateFile(s, GENERIC_READ|GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 			dbf("serial::open handle=%d <%s>\n", (int)handle, s);
+			if(handle==INVALID_HANDLE_VALUE)
+				return 1;
 			dcb.BaudRate = 9600;
 			dcb.ByteSize = 8;
 			SetCommState(handle, &dcb);
@@ -45,6 +47,9 @@ device_s *create_device_serial(const char *name)
 {
 	dbf("create_device_serial(%s)\n", name);
 	serial *p = new serial();
-	p->open(name);
+	if(p->open(name)) {
+		delete p;
+		return NULL;
+	}
 	return p;
 }
